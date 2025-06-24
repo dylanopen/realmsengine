bf2c3a3898ff753226cd6456ad267e9f97f1e286
dylan <opendylan@proton.me>
2025-06-24 13:29:42 +0100

    add Size component

---
+++ src/org/realmsengine/component/Size.java
@@ -0,0 +1,9 @@
+package org.realmsengine.component;
+
+public interface Size {
+    double getWidth();
+    double getHeight();
+    void setWidth(double width);
+    void setHeight(double height);
+    void setSize(double width, double height);
+}
