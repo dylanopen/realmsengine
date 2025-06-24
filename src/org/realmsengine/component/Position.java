4b727bb5ee650b7bbe4a6569aa029ecc14de3aea
dylan <opendylan@proton.me>
2025-06-24 13:29:23 +0100

    add Position component

---
+++ src/org/realmsengine/component/Position.java
@@ -0,0 +1,9 @@
+package org.realmsengine.component;
+
+public interface Position {
+    double getX();
+    double getY();
+    void setX(double x);
+    void setY(double y);
+    void setPosition(double x, double y);
+}
