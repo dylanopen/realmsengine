2d440f4e14adee55e19529217d712952bbc712dc
dylan <opendylan@proton.me>
2025-06-25 10:38:41 +0100

    define SizeComponent addWidth, addHeight and addSize methods

--- src/org/realmsengine/component/SizeComponent.java
+++ src/org/realmsengine/component/SizeComponent.java
@@ -2,11 +2,27 @@ package org.realmsengine.component;
 
 public interface SizeComponent {
     double getWidth();
+
     double getHeight();
+
     void setWidth(double width);
+
     void setHeight(double height);
+
     default void setSize(double width, double height) {
         setWidth(width);
         setHeight(height);
+    }
+
+    default void addWidth(double delta) {
+        setWidth(getWidth() + delta);
+    }
+    default void addHeight(double delta) {
+        setHeight(getHeight() + delta);
+    }
+
+    default void addSize(double deltaX, double deltaY) {
+        addWidth(deltaX);
+        addHeight(deltaY);
     }
 }
