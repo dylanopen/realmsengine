a02d5375380c328ad36ababd2ddab33a3e063ffd
dylan <opendylan@proton.me>
2025-06-25 10:05:31 +0100

    define PositionComponent addX, addY and addPosition methods

--- src/org/realmsengine/component/PositionComponent.java
+++ src/org/realmsengine/component/PositionComponent.java
@@ -2,11 +2,27 @@ package org.realmsengine.component;
 
 public interface PositionComponent {
     double getX();
+
     double getY();
+
     void setX(double x);
+
     void setY(double y);
+
     default void setPosition(double x, double y) {
         setX(x);
         setY(y);
+    }
+
+    default void addX(double delta) {
+        setX(getX() + delta);
+    }
+    default void addY(double delta) {
+        setY(getY() + delta);
+    }
+
+    default void addPosition(double deltaX, double deltaY) {
+        addX(deltaX);
+        addY(deltaY);
     }
 }
